c7efc8a7634576aca4e18f8cac8bc4558f0d11e9
Daniel Brumazzi <brumazzi_daniel@yahoo.com.br>
2016-06-08 23:45:33 -0400

    Review errors

--- libs/utils.js
+++ libs/utils.js
@@ -1,21 +1,26 @@
 var canv = null;
 var ctx = null;
-var _fps = null;
+var fps = null;
 
-const COLISION_TOP	=	1;
-const COLISION_LEFT	=	2;
-const COLISION_RIGHT	=	4;
-const COLISION_BOTTOM	=	8;
+const COLLISION_TOP	=	1;
+const COLLISION_LEFT	=	2;
+const COLLISION_RIGHT	=	4;
+const COLLISION_BOTTOM	=	8;
 
-function fps(_fps_){
+function set_fps(_fps_){
 	if(!_fps_)
-		return _fps;
-	_fps = _fps_;
+		return fps;
+	fps = 1000/_fps_;
+}
+
+function __clear__(){
+	ctx.fillStyle = "#000000";
+	ctx.fillRect(0, 0, canv.width, canv.height);
 }
 
 function __draw__(obj){
 	ctx.fillStyle = obj.color;
-	ctx.fillRect(obj.x, obj.x, obj.width, obj.height);
+	ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
 }
 
 function __update__(obj, data){
@@ -27,45 +32,48 @@ function __update__(obj, data){
 	obj.show = data.show;
 }
 
-function __colision__(obj1, obj2){
+function __collision__(obj1, obj2){
 	function colision(){
 		return (
 				(obj1.x+obj1.width >= obj2.x) &&
 				(obj1.x <= obj2.x+obj2.width) &&
 				(obj1.y+obj1.height >= obj2.y) &&
-				(obj1.y <= obj2.y+obje.height)
+				(obj1.y <= obj2.y+obj2.height)
 			) ? true : false;
 	}
 
 	if(!colision())
 		return false;
 
-	if(obj1.y + obj1.height <= obj2.y){ // Top Colision
+	if(obj1.y + obj1.height >= obj2.y){ // Top Colision
 		var zero = obj1.x+obj1.width-obj2.x;
-		var valu = obj2.x+obj2.width;
+		var valu = obj2.x+obj2.width-obj2.x+obj1.width;
 		var perc = zero*100/valu;
 		return [COLISION_TOP, perc];
-	}
-	if(obj1.y >= obj2.y+obj2.height){ // Bottom Colision
+	}/*else TODO HACK
+	if(obj1.y <= obj2.y+obj2.height){ // Bottom Colision
 		var zero = obj1.x+obj1.width-obj2.x;
 		var valu = obj2.x+obj2.width;
 		var perc = zero*100/valu;
 		return [COLISION_BOTTOM, perc];
-	}
-	if(obj1.x + obj1.width <= obj2.x){ // Left Colision
+	}else
+	if(obj1.x + obj1.width >= obj2.x){ // Left Colision
 		var zero = obj1.y+obj1.height-obj2.y;
 		var valu = obj2.y+obj2.height;
 		var perc = zero*100/valu;
 		return [COLISION_LEFT, perc];
-	}
-	if(obj1.x >= obj2.x + obj2.width){ // Right Colision
+	}else
+	if(obj1.x <= obj2.x + obj2.width){ // Right Colision
 		var zero = obj1.y+obj1.height-obj2.y;
 		var valu = obj2.y+obj2.height;
 		var perc = zero*100/valu;
 		return [COLISION_RIGHT, perc];
-	}
-}
+	}*/
 
+	return true;
+}
+// 10 10
+// 10 20
 function Cube(x, y,width, height, color){
 	this.width = width;
 	this.height = height;
@@ -73,6 +81,14 @@ function Cube(x, y,width, height, color){
 	this.y = y;
 	this.color = color;
 	this.show = true;
+
+	this.collision = function(obj){
+		return __collision__(this, obj);
+	};
+
+	this.draw = function(){
+		__draw__(this);
+	}
 
 	return this;
 }
@@ -112,4 +128,20 @@ function Hero(x,y,width, height, color){
 	}
 
 	return this;
+}
+
+var utils_start = function(callback){
+	var back = window.onload;
+
+	window.onload = function(){
+		var main = document.body;
+		main.innerHTML = "<canvas id=\"canv\" width='"+window.innerWidth+"' height='"+window.innerHeight+"'></canvas>";
+
+		canv = document.getElementById('canv');
+		ctx = canv.getContext("2d");
+		__clear__();
+		if(back)
+			back();
+		callback();
+	}
 }
